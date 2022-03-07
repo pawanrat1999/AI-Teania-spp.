cb8496682779a7869f1bab131f809c1fe86529c3
pawanrat1999 <101077084+pawanrat1999@users.noreply.github.com>
2022-03-07 14:39:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def get_detection_folder():
 
 if __name__ == '__main__':
 
-    st.title('TH-DDC-AI-Teania Detector')
+    st.title('ปัญญาประดิษฐ์วินิจฉัยพยาธิตัวคืด')
     st.subheader('develop by https://kasira.biz')
 
     parser = argparse.ArgumentParser()
